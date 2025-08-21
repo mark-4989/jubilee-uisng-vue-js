7f2d3e33c50fb4684d34bc3170393087db501658
mark <ngarimark27@gmail.com>
2025-08-21 16:41:01 +0300

    imported header and footer to app.vue

--- school-ui/src/App.vue
+++ school-ui/src/App.vue
@@ -1,11 +1,44 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <TheHeader />
+    <main class="main-content">
+      <router-view /> <!-- This is where the page content (Home, About, etc.) will be injected -->
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.main-content {
+  flex: 1; /* Makes the content area grow to push footer down */
+}
+/* Global reset styles */
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+</style>
