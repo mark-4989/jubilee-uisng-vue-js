0c056bd13c4a1346f565ac7a5a7928dd5887d8b2
mark <ngarimark27@gmail.com>
2025-08-21 16:38:50 +0300

    footer greated

---
+++ school-ui/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; {{ new Date().getFullYear() }} {{ schoolName }}. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  data() {
+    return {
+      schoolName: 'Sunshine Elementary School'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #f5f5f5;
+  padding: 2rem 0;
+  text-align: center;
+  margin-top: auto; /* Pushes footer to bottom if using flexbox on App.vue */
+}
+</style>
